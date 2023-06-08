bf2b0fb582c5cebfd5cafb0ef1ba96ebb5c2010e
Nathan <nslee333@gmail.com>
2023-06-08 07:05:45 -0700

    Dev comments

--- hello.go
+++ hello.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("World"))
 }
+
+// & Not sure if I have everything is installed okay
+
+// ^ Still chasing my tail with install vscode error
