a5a63de9f571ad5303d54b3b23ed9d4bf437fd66
Nathan <nslee333@gmail.com>
2023-06-09 15:03:21 -0700

    Added benchmark for Repeat

--- for/repeat_test.go
+++ for/repeat_test.go
@@ -13,7 +13,10 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 
-	// t.Run("Should iterate", func(t *testing.T) {
+}
 
-	// })
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
 }
