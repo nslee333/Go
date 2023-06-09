1bebe2c0344275b22580d5dee48187f8d765ff54
Nathan <nslee333@gmail.com>
2023-06-09 14:29:49 -0700

    Added example test for docs

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,15 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
 
 func TestAddr(t *testing.T) {
 	sum := Add(2, 2)
