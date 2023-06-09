fcecdc05fb2f6ac99c4e67d3003179b7785ce807
Nathan <nslee333@gmail.com>
2023-06-09 14:08:57 -0700

    Extracted greetingPrefix to separate function

--- hello.go
+++ hello.go
@@ -13,14 +13,21 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
+	prefix := greetingPrefix(language)
 
-	return englishHelloPrefix + name
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
