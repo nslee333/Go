0683430ef6c08352271f71efc46c449e5392291b
Nathan <nslee333@gmail.com>
2023-06-10 08:29:13 -0700

    Added struct type

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,14 +3,17 @@ package main
 import "testing"
 
 func TestParameter(t *testing.T) {
-	got := Parameter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Parameter(rectangle)
 	want := 40.0
 
 	assertCorrectResponse(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+
+	got := Area(rectangle)
 	want := 100.0
 
 	assertCorrectResponse(t, got, want)
