079dd9ac5b1612a3cc8eaee0efb300cdebe6068a
Nathan <nslee333@gmail.com>
2023-06-10 14:34:32 -0700

    Created DictionaryErr type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,16 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("word already exists")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("word already exists")
 )
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
