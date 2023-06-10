62a38c182dbdfce0f642999e4e4456371dd6eace
Nathan <nslee333@gmail.com>
2023-06-10 09:10:08 -0700

    Added methods to struct and interface, + tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -7,22 +7,29 @@ func TestParameter(t *testing.T) {
 	got := Parameter(rectangle)
 	want := 40.0
 
-	assertCorrectResponse(t, got, want)
+	if got != want {
+		t.Errorf("Got %f want %f", got, want)
+	}
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 
-	got := Area(rectangle)
-	want := 100.0
-
-	assertCorrectResponse(t, got, want)
-}
-
-func assertCorrectResponse(t *testing.T, got, want float64) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("Got %.2f want %.2f", got, want)
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
 }
