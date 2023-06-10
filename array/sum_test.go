0ceef7f6e6f0ada6b5906f49b886c0852a15abfb
Nathan <nslee333@gmail.com>
2023-06-09 17:34:31 -0700

    test for sum.

--- array/sum_test.go
+++ array/sum_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
 	want := 15
 
 	if got != want {
-		t.Errorf("Got %v want %v", got, want)
+		t.Errorf("Got %v want %v gave %v", got, want, numbers)
 	}
 }
