84753cd20f5fe2b1fbeba12cc83fb7189bb22ac8
Nathan <nslee333@gmail.com>
2023-06-09 19:25:16 -0700

    Wrapping up sum and test

--- array/sum_test.go
+++ array/sum_test.go
@@ -25,4 +25,36 @@ func TestSumAll(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Got %v want %v", got, want)
 	}
+
+	checkSums(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("Make sums of the slices", func(t *testing.T) {
+		numbersToSum1 := []int{1, 2, 3, 4, 5}
+		numbersToSum2 := []int{1, 2, 3, 4, 5}
+		numbersToSum3 := []int{1, 2, 3, 4, 5}
+
+		got := SumAllTails(numbersToSum1, numbersToSum2, numbersToSum3)
+		want := []int{14, 14, 14}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
+}
+
+func checkSums(t *testing.T, got, want []int) {
+	t.Helper()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v want %v", got, want)
+	}
 }
