93a41969420c62a3bca61dca71c39a99babf10db
Nathan <nslee333@gmail.com>
2023-06-09 22:26:06 -0700

    Extracted assertion to own function

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -6,14 +6,18 @@ func TestParameter(t *testing.T) {
 	got := Parameter(10.0, 10.0)
 	want := 40.0
 
-	if got != want {
-		t.Errorf("Got %.2f want %.2f", got, want)
-	}
+	assertCorrectResponse(t, got, want)
 }
 
 func TestArea(t *testing.T) {
 	got := Area(10.0, 10.0)
 	want := 100.0
+
+	assertCorrectResponse(t, got, want)
+}
+
+func assertCorrectResponse(t *testing.T, got, want float64) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("Got %.2f want %.2f", got, want)
