41fedf049a86d021a1a2f74a8692b02911082aec
Nathan <nslee333@gmail.com>
2023-06-10 19:15:06 -0700

    Fixed broken test

--- mock/countdown_test.go
+++ mock/countdown_test.go
@@ -9,7 +9,7 @@ func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	spySleeper := &SpySleeper{}
 
-	Countdown(buffer, &SpySleeper{})
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := "3\n2\n1\nGo!\n"
@@ -22,11 +22,3 @@ func TestCountdown(t *testing.T) {
 		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
-
-// type SpySleeper struct {
-// 	Calls int
-// }
-
-// func (s *SpySleeper) Sleep() {
-// 	s.Calls++
-// }
