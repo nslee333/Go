ec3a793019d8b194d4c9da74d0265e67e633ef6f
Nathan <nslee333@gmail.com>
2023-06-10 18:06:47 -0700

    WIP: Mocking

---
+++ mock/countdown.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	Countdown(os.Stdout)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprint(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
