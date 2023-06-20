f8cbf061495d4347795624de5d58e13ec429ff36
Nathan <nslee333@gmail.com>
2023-06-20 12:47:33 -0700

    WIP: roman numerals

--- roman_numerals/numeral.go
+++ roman_numerals/numeral.go
@@ -7,7 +7,18 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
+var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -35,4 +46,30 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+func convertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if i+1 < len(roman) && symbol == 'I' {
+			nextSymbol := roman[i+1]
+
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
 }
