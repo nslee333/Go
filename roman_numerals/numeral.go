04851b5bcf82686e4ff36ffdb93f6b8bf3c0b162
Nathan <nslee333@gmail.com>
2023-06-20 11:19:10 -0700

    Refactor

--- roman_numerals/numeral.go
+++ roman_numerals/numeral.go
@@ -4,22 +4,20 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := arabic; i > 0; i-- {
-
-		if arabic == 4 {
-			result.WriteString("IV")
-			break
-		} else if arabic == 5 {
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
 			result.WriteString("V")
-			break
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
 		}
-		result.WriteString("I")
 	}
 
 	return result.String()
