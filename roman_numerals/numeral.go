1e13957c583f4f30a561fa44338b01461e225dfe
Nathan <nslee333@gmail.com>
2023-06-20 11:05:26 -0700

    added for 3

--- roman_numerals/numeral.go
+++ roman_numerals/numeral.go
@@ -4,6 +4,8 @@ func ConvertToRoman(arabic int) string {
 
 	if arabic == 2 {
 		return "II"
+	} else if arabic == 3 {
+		return "III"
 	}
 	return "I"
 }
--- roman_numerals/numeral_test.go
+++ roman_numerals/numeral_test.go
@@ -10,8 +10,8 @@ func TestRomanNumerals(t *testing.T) {
 		Want        string
 	}{
 		{"1 gets converted to I", 1, "I"},
-		{"1 gets converted to II", 2, "II"},
-		{},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
 	}
 
 	for _, test := range cases {
