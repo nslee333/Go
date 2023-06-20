a61720ce7271b06b1372450694ef076c854bc8e3
Nathan <nslee333@gmail.com>
2023-06-20 11:07:27 -0700

    Refactor to a loop

--- roman_numerals/numeral.go
+++ roman_numerals/numeral.go
@@ -1,11 +1,14 @@
 package main
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
 
-	if arabic == 2 {
-		return "II"
-	} else if arabic == 3 {
-		return "III"
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
 	}
-	return "I"
+
+	return result.String()
 }
