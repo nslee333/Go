5683ed06045bd35d8e47a02090d0872dcea1fc54
Nathan <nslee333@gmail.com>
2023-06-21 12:56:53 -0700

    Refactor

--- math/clockface.go
+++ math/clockface.go
@@ -18,14 +18,6 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
-func simpleTime(hours, minutes, seconds int) time.Time {
-	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
-}
-
-func testName(t time.Time) string {
-	return t.Format("15:04:05")
-}
-
 func secondHandPoint(t time.Time) Point {
 	angle := secondsInRadians(t)
 	x := math.Sin(angle)
