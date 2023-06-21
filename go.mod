8ebf3247e73f02f13d972ecfab11ade196e24842
Nathan <nslee333@gmail.com>
2023-06-20 19:50:49 -0700

    WIP: clockface

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.20
+
+require github.com/quii/learn-go-with-tests v0.0.0-20230526105308-3a6289ffe064
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -1,1 +1,19 @@
-package main
+package clockface_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/quii/learn-go-with-tests/math/v1/clockface"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
