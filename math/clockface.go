38206b99a9207148e3b1b1fe814bfa1366e7fe02
Nathan <nslee333@gmail.com>
2023-06-21 12:43:26 -0700

    WIP: Math

--- math/clockface.go
+++ math/clockface.go
@@ -15,5 +15,14 @@ func SecondHand(t time.Time) Point {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi
+	// return float64(t.Second()) * (math.Pi / 30)
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -4,37 +4,25 @@ import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/quii/learn-go-with-tests/math/v1/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	// tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-	// want := clockface.Point{X: 150, Y: 150 + 90}
-	// got := clockface.Secondhand(tm)
-
-	// if got != want {
-	// 	t.Errorf("got %v, wanted %v", got, want)
-	// }
-}
-
 func TestSecondsInRadians(t *testing.T) {
-	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := secondsInRadians(thirtySeconds)
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
 
-	if got != want {
-		t.Fatalf("wanted %v radians, but got %v", want, got)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("wanted %v radians, but got %v", c.angle, got)
+			}
+		})
 	}
 }
