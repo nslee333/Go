313ad33874eeda61cc65868ea35910a7dc9ea9f4
Nathan <nslee333@gmail.com>
2023-06-26 15:58:05 -0700

    Added test and func sig

--- math/clockface_test.go
+++ math/clockface_test.go
@@ -84,6 +84,24 @@ func TestMinuteHandPoint(t *testing.T) {
 	}
 }
 
+func TestHoursInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hoursInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
 func roughlyEqualFloat64(a, b float64) bool {
 	const equalityThreshold = 1e-7
 	return math.Abs(a-b) < equalityThreshold
