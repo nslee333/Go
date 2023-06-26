c22b2b3eab0bf58f2f3f76a55e65ea45db76d751
Nathan <nslee333@gmail.com>
2023-06-26 15:30:05 -0700

    Moving around stuff

--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -12,6 +12,7 @@ import (
 
 type SVG struct {
 	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
@@ -21,17 +22,17 @@ type SVG struct {
 	Line    []Line   `xml:"line"`
 }
 
-type Circle struct {
-	Cx float64 `xml:"cx,attr"`
-	Cy float64 `xml:"cy,attr"`
-	R  float64 `xml:"r,attr"`
-}
-
 type Line struct {
 	X1 float64 `xml:"x1,attr"`
 	Y1 float64 `xml:"y1,attr"`
 	X2 float64 `xml:"x2,attr"`
 	Y2 float64 `xml:"y2,attr"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
 }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
