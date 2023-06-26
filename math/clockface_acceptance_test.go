8e0d22ebb8fe8b06c4ae88ed796c57828b03d2ff
Nathan <nslee333@gmail.com>
2023-06-26 12:36:11 -0700

    another acceptance test

--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -1,6 +1,8 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
 	"strings"
 	"testing"
 	"time"
@@ -8,18 +10,48 @@ import (
 	"github.com/quii/learn-go-with-tests/math/v7/clockface"
 )
 
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	b := bytes.Buffer{}
 
-	var b strings.Builder
 	clockface.SVGWriter(&b, tm)
-	got := b.String()
 
-	want := `<line x1="150" y1="150" x2="150.000" y2="60.000"`
+	svg := SVG{}
 
-	if !strings.Contains(got, want) {
-		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
+	xml.Unmarshal(b.Bytes(), &svg)
+	want := Line{150, 150, 150, 60}
+
+	for _, line := range svg.Line {
+		if line == want {
+			return
+		}
 	}
+
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
 }
 
 func TestSVGWriterAt30Seconds(t *testing.T) {
@@ -34,4 +66,51 @@ func TestSVGWriterAt30Seconds(t *testing.T) {
 	if !strings.Contains(got, want) {
 		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
 	}
+}
+
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
