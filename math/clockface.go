0d1549d746e3d583a2d9609665fb616c01bb1f54
Nathan <nslee333@gmail.com>
2023-06-26 14:53:22 -0700

    Filled in logic for minutesInRadians

--- math/clockface.go
+++ math/clockface.go
@@ -22,3 +22,8 @@ func secondHandPoint(t time.Time) Point {
 
 	return Point{x, y}
 }
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
