d9d747c27e4d30fa3ea9e02c7d5e05a3b145d9a2
Nathan <nslee333@gmail.com>
2023-06-26 15:41:19 -0700

    updated import to fix bug :eyeroll:

--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -3,11 +3,10 @@ package clockface_test
 import (
 	"bytes"
 	"encoding/xml"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/quii/learn-go-with-tests/math/v7/clockface"
+	"github.com/quii/learn-go-with-tests/math/v9/clockface"
 )
 
 type SVG struct {
@@ -35,40 +34,6 @@ type Circle struct {
 	R  float64 `xml:"r,attr"`
 }
 
-func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	b := bytes.Buffer{}
-
-	clockface.SVGWriter(&b, tm)
-
-	svg := SVG{}
-
-	xml.Unmarshal(b.Bytes(), &svg)
-	want := Line{150, 150, 150, 60}
-
-	for _, line := range svg.Line {
-		if line == want {
-			return
-		}
-	}
-
-	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
-}
-
-func TestSVGWriterAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	var b strings.Builder
-	clockface.SVGWriter(&b, tm)
-	got := b.String()
-
-	want := `<line x1="150" y1="150" x2="150.000" y2="240.000"`
-
-	if !strings.Contains(got, want) {
-		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
-	}
-}
-
 func TestSVGWriterSecondHand(t *testing.T) {
 	cases := []struct {
 		time time.Time
@@ -93,13 +58,13 @@ func TestSVGWriterSecondHand(t *testing.T) {
 			xml.Unmarshal(b.Bytes(), &svg)
 
 			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the second hand %+v, in the SVG lines %+v", c.line, svg.Line)
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
 			}
 		})
 	}
 }
 
-func TestSVGWriterMinuteHand(t *testing.T) {
+func TestSVGWriterMinutedHand(t *testing.T) {
 	cases := []struct {
 		time time.Time
 		line Line
@@ -109,6 +74,7 @@ func TestSVGWriterMinuteHand(t *testing.T) {
 			Line{150, 150, 150, 70},
 		},
 	}
+
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
 			b := bytes.Buffer{}
@@ -118,7 +84,7 @@ func TestSVGWriterMinuteHand(t *testing.T) {
 			xml.Unmarshal(b.Bytes(), &svg)
 
 			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the minute line %+v, in SVG lines %+v", c.line, svg.Line)
+				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
 			}
 		})
 	}
