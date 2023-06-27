2daedf1acaf5d5b0727f9bbe88495b61bc624c1f
Nathan <nslee333@gmail.com>
2023-06-27 14:59:58 -0700

    Added test and RenderIndex

--- templates/blogrenderer/renderer.go
+++ templates/blogrenderer/renderer.go
@@ -3,6 +3,7 @@ package blogrenderer
 import (
 	"embed"
 	"io"
+	"strings"
 	"text/template"
 )
 
@@ -31,6 +32,24 @@ func NewPostRenderer() (*PostRenderer, error) {
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
 	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
