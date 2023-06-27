2729bfe0e82a076a8ad765f41e93c2c75b43919f
Nathan <nslee333@gmail.com>
2023-06-27 12:39:19 -0700

    Added approval test, approval file

--- templates/blogrenderer/renderer_test.go
+++ templates/blogrenderer/renderer_test.go
@@ -3,6 +3,8 @@ package blogrenderer
 import (
 	"bytes"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRenderer(t *testing.T) {
@@ -17,19 +19,11 @@ func TestRenderer(t *testing.T) {
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := Render(&buf, aPost)
-
-		if err != nil {
+		if err := Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
-		got := buf.String()
-		want := `<h1>hello world</h1>
-<p>This is a description</p>
-Tags: <ul><li>go</li><li>tdd></li></ul>`
+		approvals.VerifyString(t, buf.String())
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
 	})
 }
