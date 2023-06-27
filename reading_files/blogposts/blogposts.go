a8b54d34c5d53467757128cc6e5eebe7d77f6cad
Nathan <nslee333@gmail.com>
2023-06-27 10:38:30 -0700

    Body field and test

--- reading_files/blogposts/blogposts.go
+++ reading_files/blogposts/blogposts.go
@@ -2,6 +2,8 @@ package blogposts
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"io/fs"
 	"strings"
@@ -16,6 +18,7 @@ const (
 type Post struct {
 	Title, Description string
 	Tags               []string
+	Body               string
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -53,9 +56,21 @@ func newPost(postBody io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
+	title := readMetaLine(titleSeparator)
+	description := readMetaLine(descriptionSeparator)
+	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+
+	scanner.Scan()
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	body := strings.TrimSuffix(buf.String(), "\n")
+
 	return Post{
-		Title:       readMetaLine(titleSeparator),
-		Description: readMetaLine(descriptionSeparator),
-		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        body,
 	}, nil
 }
