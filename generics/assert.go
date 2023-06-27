f560312c29897388bfafd2ec943aa47254f4ec24
Nathan <nslee333@gmail.com>
2023-06-27 16:25:10 -0700

    Added more assertion helpers

--- generics/assert.go
+++ generics/assert.go
@@ -27,3 +27,17 @@ func AssertNotEqual(t *testing.T, got, want interface{}) {
 		t.Error("didn't want %+v", got)
 	}
 }
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
