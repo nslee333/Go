f5f9f732b608b334cb5c4cff7c0961c61b85a5f4
Nathan <nslee333@gmail.com>
2023-06-28 11:00:58 -0700

    Extracted reduce functions to own file

--- array/sum.go
+++ array/sum.go
@@ -29,11 +29,3 @@ func SumAllTails(numbersToSum ...[]int) []int {
 
 	return Reduce(numbersToSum, sumTail, []int{})
 }
-
-func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
-	var result = initialValue
-	for _, x := range collection {
-		result = accumulator(result, x)
-	}
-	return result
-}
